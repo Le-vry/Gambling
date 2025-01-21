f8b5d03ae9d5bb158571ab2229b6046e683bfcf4
Elliot Lidman <elllid@elevad.umea.se>
2025-01-21 11:33:08 +0100

    message

--- src/routes/Search/+layout.svelte
+++ src/routes/Search/+layout.svelte
@@ -52,9 +52,10 @@
 
 <main>
     <header>
-        <div class="return"><a class="back" href="{base}">⟵Back</a></div>
+        <div class="return"><a class="back" href="{base}/Search">⟵Back</a></div>
         <form on:submit|preventDefault={handlesubmit(search)}>
             <input type="text" placeholder="Sök pokemon" bind:value={search} />
+            {console.log(search)}
         </form>
         <!-- svelte-ignore a11y-click-events-have-key-events -->
         <!-- svelte-ignore a11y-no-static-element-interactions -->
